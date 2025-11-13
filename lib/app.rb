d601c94a21adad72915216dbfd901eb60e354d06
Mark Connell <mark@connell.uk>
2025-11-13 13:05:58 +0000

    serve up a template on the root dir

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 require "roda"
+require_relative "html_extractor"
 
 class App < Roda
+  plugin :public, root: "public"
   plugin :json
 
   route do |r|
+    r.root do
+      File.read("public/index.html")
+    end
+
+    r.public
+
     r.get "up" do
       { status: "ok" }
     end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,6 +4,15 @@ require "spec_helper"
 require "json"
 
 RSpec.describe "Health check" do
+  describe "GET /" do
+    it "returns 200 OK and renders a template" do
+      get "/"
+
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("EML to HTML")
+    end
+  end
+
   describe "GET /up" do
     it "returns 200 OK and JSON body" do
       get "/up"
